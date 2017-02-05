e247ea454bfd38cc2ce3652f38980254acf8a276
Manuel Jiménez <cotoprixve@gmail.com>
2017-02-05 08:38:56 -0400

    Add files via upload
    slim route and data model

---
+++ app/app/model.php
@@ -0,0 +1,413 @@
+<?php
+namespace App\Model;
+
+use App\Lib\Response;
+use App\Lib\Constants AS CT;
+use Monolog\Logger;
+use MongoDB;
+
+class AppModel
+{
+    private $conn;
+    private $response;
+    private $crypt;
+    private $logger;
+    
+    public function __CONSTRUCT()
+    {
+        $this->conn = new MongoDB\Driver\Manager( CT\DB_HOST );
+        $this->response = new Response();
+        # INITIALIZED THE CLASS TO ENCRYPT AND DESENCRIPT THE IDENTIFIERS
+//        $this->crypt = new CRYPTO\Crypter(Constantes::getConstantes()['ENCRYPTION_KEY'],Constantes::getConstantes()['ENCRYPTION_IV']);
+//        $this->logger = $logger;
+    }
+    
+    private function error_msg($filename, $e)
+    {
+        echo "The $filename script has experienced an error.\n<br>";
+        echo "It failed with the following exception:\n<br>";
+        echo "Exception:", $e->getMessage(), "\n<br>";
+        echo "In file:", $e->getFile(), "\n<br>";
+        echo "On line:", $e->getLine(), "\n<br>";
+    }
+
+    public function isLoggin()
+    {
+        return ( isset( $_SESSION['account'] ) ) ? true: false;
+    }
+
+    public function inicio()
+    {
+        try {
+            
+            # Set table
+            $table = CT\TABLE_PRODUCTS;
+            
+            # Set up query
+            $query = new MongoDB\Driver\Query([]); 
+
+            # Run the query and position record
+            $rows = $this->conn->executeQuery( $table, $query );
+
+            # Decode form object
+            $cursor = json_decode( json_encode( $rows->toArray() ), true );
+
+            $arr = array(
+                "cursor" => $cursor,
+
+                "num_docs" => count( $cursor )
+
+            );
+
+            return $arr;
+
+        } catch ( MongoDB\Driver\Exception\Exception $e ) {
+
+            # Filename with errors
+            $filename = basename(__FILE__);
+
+            # Error message
+            self::error_msg( $filename, $e );
+
+        }
+    }
+
+    public function start()
+    {
+        try {
+            
+            # Set table
+            $table = CT\TABLE_USERS;
+            
+            # Set up query
+            $query = new MongoDB\Driver\Query([]); 
+
+            # Run the query and position record
+            $rows = $this->conn->executeQuery( $table, $query );
+
+            foreach ($rows as $row) {
+
+                if ( isset( $row->address ) )
+                    $address = $row->address;                
+            
+            }
+
+            # Set up filter
+            $filter = [ 'user_id' => new MongoDB\BSON\ObjectID( $_SESSION["account"] ) ]; 
+            
+            # Set up query
+            $query = new MongoDB\Driver\Query( $filter );                        
+            
+            # Run the query and position record
+            $rows = $this->conn->executeQuery( CT\TABLE_ORDERS, $query );
+            
+            # Decode form object
+            $something = json_decode( json_encode( $rows->toArray() ), true );
+
+            return array( 'address'=>$address, 'something' => $something );
+
+        } catch ( MongoDB\Driver\Exception\Exception $e ) {
+            
+            # Filename with errors
+            $filename = basename(__FILE__);
+            
+            # Error message
+            self::error_msg( $filename, $e );
+
+        }
+
+    }
+
+    public function orders($data)
+    {
+        try {
+
+            # Set table
+            $table = CT\TABLE_ORDERS;
+            
+            # Set up filter
+            $filter = array( '_id' => new MongoDB\BSON\ObjectID( $data ) );
+
+            # Set up query
+            $query = new MongoDB\Driver\Query( $filter );
+            
+            # Run the query and position record
+            $cursor = $this->conn->executeQuery( $table, $query );
+
+            # Read cursor
+            foreach ( $cursor as $obj ) {
+                $ids = $obj->ids;
+                $ids = explode( ";", $ids );
+            }
+
+            $_ids = array();
+            
+            # Read ids
+            foreach ( $ids as $separateIds ) {
+                $_ids[] = $separateIds instanceof \MongoDB\BSON\ObjectID ? $separateIds : new MongoDB\BSON\ObjectID( $separateIds );
+            }
+            
+            # Count values
+            $how_many = array_count_values( $ids );
+            
+            # Set up filter
+            $filter = array( '_id' => array( '$in' => $_ids ) );
+            
+            # Set up query            
+            $query = new MongoDB\Driver\Query( $filter );
+            
+            # Run the query and position record
+            $cursor = $this->conn->executeQuery( CT\TABLE_PRODUCTS, $query );
+            
+            # Decode form object
+            $thisSearch = json_decode( json_encode( $cursor->toArray() ), true );
+            
+            # Return
+            return array( 'thisSearch' => $thisSearch, 'how_many' => $how_many );
+
+        } catch ( MongoDB\Driver\Exception\Exception $e ) {
+            
+            # Filename with errors
+            $filename = basename(__FILE__);
+            
+            # Error message
+            self::error_msg( $filename, $e );
+
+        }
+
+    }
+
+    public function login($data)
+    {
+        try {
+
+            # Set table
+            $table = CT\TABLE_USERS;
+
+            $username = (isset( $data["username"] ) ? $data["username"] : $username = null);
+
+            $password = (isset( $data["password"] ) ? md5( $data["password"] ) : $password = null);
+
+            # Set up filter
+            $filter = array( 'username' => $username, "password" => $password );
+            
+            # Set up query
+            $query = new MongoDB\Driver\Query( $filter );
+            
+            # Run the query and position record
+            $rows = $this->conn->executeQuery( $table, $query );
+            
+            # Position the record
+            $login = current( $rows->toArray() );
+            
+            # Response query
+            if ( !empty( $login ) ) {
+
+                $_SESSION["account"] = $login->_id;
+                
+                $_SESSION['role'] = $login->role;
+                
+                return true;
+
+            } else {
+                
+                $_SESSION["account"] = null;
+                
+                $_SESSION['role'] = null;
+                
+                return false;
+
+            }
+
+        } catch ( MongoDB\Driver\Exception\Exception $e ) {
+            
+            # Filename with errors
+            $filename = basename(__FILE__);
+            
+            # Error message
+            self::error_msg( $filename, $e );
+
+        }
+    }
+
+    public function saveaddress( $data )
+    {
+
+        try {
+            
+            # Set table
+            $table = CT\TABLE_USERS;
+            
+            #Prepare writing (bulkwrite)
+            $bulk = new MongoDB\Driver\BulkWrite;
+            
+            $bulk->update(
+                ['_id' => new MongoDB\BSON\ObjectID( $_SESSION["account"] )],
+                ['$set' => ['address' => $data["u_address"] ]]
+            );
+            
+            # Save back to the database            
+            $res = $this->conn->executeBulkWrite( $table , $bulk );
+
+            # Response query
+            if ( $res ) {
+
+                return array('msg' => sprintf( CT\MSG['OKUPDATEADDRESS'], 'Dirección' ) );
+
+            }else{
+
+                return array('msg' => CT\ERROR[501] );
+
+            }
+                
+        } catch ( MongoDB\Driver\Exception\Exception $e ) {
+            
+            # Filename with errors
+            $filename = basename(__FILE__);
+            
+            # Error message
+            self::error_msg( $filename, $e );
+
+        }
+
+    }
+
+    public function manage()
+    {
+        try {
+            
+            # Set table
+            $table = CT\TABLE_PRODUCTS;
+            
+            # Set up query
+            $query = new MongoDB\Driver\Query([]); 
+            
+            # Run the query and position record
+            $rows = $this->conn->executeQuery( $table, $query );
+            
+            return array( 'something' => $rows );
+
+        } catch ( MongoDB\Driver\Exception\Exception $e ) {
+            
+            # Filename with errors
+            $filename = basename(__FILE__);
+            
+            # Error message
+            self::error_msg( $filename, $e );
+
+        }
+
+    }
+  
+    public function manage_up( $id )
+    {
+        try {
+            
+            # Set table
+            $table = CT\TABLE_PRODUCTS;
+            
+            # Set up filter
+            $filter = array( '_id' => new MongoDB\BSON\ObjectID( $data["update"] ) );
+            
+            # Set up query
+            $query = new MongoDB\Driver\Query( $filter );
+            
+            # Run the query and position record
+            $rows = $this->conn->executeQuery( $table, $query );
+            
+            # Position the record
+            $car = current( $rows->toArray() );
+            
+            # Response query
+            if ( !empty( $car ) ) {
+
+                return array( 
+                            'id' => $car->_id,
+                            'title' => $car->title,
+                            'description' => $car->description,
+                            'price' => $car->price
+                        );
+
+            } else {
+
+                echo "No match found\n";
+            }
+
+
+        } catch ( MongoDB\Driver\Exception\Exception $e ) {
+            
+            # Filename with errors
+            $filename = basename(__FILE__);
+            
+            # Error message
+            self::error_msg( $filename, $e );
+
+        }
+
+    }
+
+    public function save( $body, $image=null )
+    {
+         try {
+            # Set table
+            $table = CT\TABLE_PRODUCTS;
+            
+            # Decode form array
+            $data = json_decode( json_encode( $body ), true );
+            
+            # Set up filter
+            $id_array = array(
+                '_id' => new MongoDB\BSON\ObjectID( $data["update"] )
+            );
+
+            // Create an array of values to update
+            foreach ( $data as $key => $value ) {
+                if ( $key == 'update' ) {
+                    unset( $data[ $key ] );
+                }else{
+                    $data[ $key ] = ( $value != '' ) ? trim( $value ) : '0';
+                }
+            }
+            
+            # Update image if upload one
+            if ( !is_null( $image ) )
+                $data['image'] = $image;
+
+            # Prepare writing (bulkwrite)
+            $bulk = new MongoDB\Driver\BulkWrite;
+            
+            # Save back to the database            
+            $bulk->update(
+
+                $id_array,
+
+                array('$set' => $data )
+
+            );
+            
+            # Run the query
+            $res = $this->conn->executeBulkWrite( $table, $bulk );
+            
+            # Response query
+            if ( $res ) {
+
+                return array( 'msg' => CT\MSG['OKUPDATE'] );
+
+            } else {
+
+                return array( 'msg' => CT\ERROR[501] );
+
+            }
+
+        } catch ( MongoDB\Driver\Exception\Exception $e ) {
+            
+            # filename with errors
+            $filename = basename(__FILE__);
+            
+            # error message
+            self::error_msg( $filename, $e );
+
+        }
+    }
+  
+}
---
+++ app/app/route.php
@@ -0,0 +1,218 @@
+<?php
+use App\Model\AppModel;
+
+$app->group('/account/', function () {
+   
+    $this->get('login', function ($req, $res, $args) {
+
+        $elp = explode('/',$req->getUri()->getBasePath());
+
+        $x['path'] = '/' . $elp[1].'/';//str_repeat("../", $nro);
+
+        return $this->renderer->render($res, 'account.phtml', $x);
+
+    })->setName('login');
+
+    $this->get('detail', function ($req, $res, $args) {
+
+        if(!isset($_SESSION['account'])) {
+
+            return $res->withRedirect($this->router->pathFor('login'));
+
+        }
+
+        $um = new AppModel();
+
+        $x = $um->start();
+
+        $nro = count(explode('/', $req->getUri()->getPath() ))  ;
+
+        $x['path'] = str_repeat("../", $nro);
+
+        return $this->renderer->render($res, 'account.phtml', $x);
+
+    })->setName('detail');
+
+    $this->get('order[/{id}]', function ($req, $res, $args) {
+
+        $um = new AppModel();
+
+        switch ($args['id']) {
+
+            case 'account':
+            case 'detail':
+
+                return $res->withRedirect($this->router->pathFor('detail'));
+
+                break;        
+
+            default:
+
+                $x = $um->orders($args['id']);
+
+                $nro = count(explode('/', $req->getUri()->getPath() ))  ;
+
+                $x['path'] = str_repeat("../", $nro);
+
+                return $this->renderer->render($res, 'orderdetail.phtml', $x);
+
+                break;
+        }
+        
+    });
+
+    $this->post('address/save', function ($req, $res) {
+
+        $um = new AppModel();
+        
+        return $res
+           ->withHeader('Content-type', 'application/json')
+           ->getBody()
+           ->write(
+            json_encode(
+                $um->saveaddress(
+                    $req->getParsedBody()
+                )
+            )
+        );
+
+    });
+    
+    $this->post('delete/{id}', function ($req, $res, $args) {
+        $um = new AppModel();
+        
+        return $res
+           ->withHeader('Content-type', 'application/json')
+           ->getBody()
+           ->write(
+            json_encode(
+                $um->Delete($args['id'])
+            )
+        );
+    });
+    
+    $this->post('account/login', function ($req, $res, $args) {
+
+        $um = new AppModel();
+
+        $arr = $um->login($req->getParsedBody());
+
+        return $res->withRedirect($this->router->pathFor('detail'));
+
+    });
+    
+    $this->get('account/logout', function ($req, $res, $args) {
+
+        session_destroy();
+
+        return $res->withRedirect($this->router->pathFor(''));
+
+    });
+    
+    
+});
+
+$app->group('/admin/', function () {
+
+    $this->get('', function($req,$res,$args) {
+
+        if(!isset($_SESSION['account'])) {
+
+            return $res->withRedirect($this->router->pathFor('login'));
+
+        }
+
+        $nro = count(explode('/', $req->getUri()->getPath() ))  ;
+
+        $x['path'] = str_repeat("../", $nro);
+
+        return $this->renderer->render( $res, 'admin.phtml', $x );
+
+    })->setName('admin');
+
+    $this->get('manage[/{params:.*}]', function($req,$res,$args) {
+
+        if(!isset($_SESSION['account'])) {
+
+            return $res->withRedirect($this->router->pathFor('login'));
+
+        }
+
+        $um = new AppModel();
+
+        $params = explode('/', $req->getAttribute('params'));
+
+        $rute = (isset($params[0]))?$params[0]:'';
+
+        switch ($rute) {
+
+            case 'update':
+
+                $x = $um->manage_up($params[1]);
+
+                $nro = count(explode('/', $req->getUri()->getPath() ))  ;
+
+                $x['path'] = str_repeat("../", $nro);
+
+                return $this->renderer->render($res, 'manage_update.phtml', $x);
+
+                break;
+            
+            default:
+
+                $x = $um->manage();
+
+                $nro = count(explode('/', $req->getUri()->getPath() ));
+
+                $elp = explode('/',$req->getUri()->getBasePath());
+
+                $x['path'] = '/' . $elp[1].'/';//str_repeat("../", $nro);
+
+                return $this->renderer->render($res, 'manage_products.phtml', $x);
+
+                break;
+
+        }
+    })->setName('manage');
+
+    $this->post('manage/update/save', function($req,$res,$args) {
+
+        $files = $req->getUploadedFiles();
+
+        if (empty($files['fileToUpload'])) {
+
+            throw new Exception('Expected a fileToUpload');
+
+        }
+
+        $uploadFileName = null;
+
+        $newfile = $files['fileToUpload'];
+        
+        $target_dir = "../uploads/";
+
+        if ($newfile->getError() === UPLOAD_ERR_OK) {
+
+            $uploadFileName = $newfile->getClientFilename();
+
+            $temp = explode('.', $uploadFileName);
+
+            $uploadFileName = round(microtime(true)) . '.' . end($temp);
+
+            $newfile->moveTo("$target_dir/$uploadFileName");
+
+        }
+
+        $um = new AppModel();
+
+        $x = $um->save(
+
+                $req->getParsedBody(),$uploadFileName
+
+            );
+
+        return $res->withRedirect($this->router->pathFor('manage'));
+        
+    });
+
+});
